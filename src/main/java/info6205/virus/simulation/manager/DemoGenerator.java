e4073ab4de861d045b9c6829551edf91cb8c7e0f
luo jiachen <luo.jiac@northeastern.edu>
2021-04-19 16:23:15 -0400

    update

--- src/main/java/info6205/virus/simulation/manager/DemoGenerator.java
+++ src/main/java/info6205/virus/simulation/manager/DemoGenerator.java
@@ -1,7 +1,7 @@
 package info6205.virus.simulation.manager;
 
 import info6205.virus.simulation.entity.*;
-import info6205.virus.simulation.entity.building.House;
+import info6205.virus.simulation.entity.building.*;
 import info6205.virus.simulation.map.SimulationMap;
 
 import java.util.ArrayList;
@@ -16,21 +16,20 @@ public class DemoGenerator extends EntityGenerator{
     public List<AreaBase> generateBuilding() {
         // Road generate
         List<RoadArea> list=new ArrayList<>();
-        for(int i=0;i<10;i++){
-            int xLU=10;
-            int yLU=10;
-            int roadWidth=5;
+        for(int i=0;i<16;i++){
+            int xLU=0;
+            int yLU=4;
+            int roadWidth=2;
             int xRD=xLU+roadWidth;
             int yRD=yLU-roadWidth;
-            RoadArea newRoad=new RoadArea(xLU+i*roadWidth,yLU,xRD+i*roadWidth,yRD,map);
-            list.add(newRoad);
+            RoadArea newRoad1=new RoadArea(xLU+i*roadWidth,yLU,xRD+i*roadWidth,yRD,map);
+            list.add(newRoad1);
             if(i!=0){
-                newRoad.linkRodaArea(list.get(i-1));
+                newRoad1.linkRodaArea(list.get(i-1));
             }
         }
 
         //House generate
-
         List<House> houses=new ArrayList<>();
         int i=0;
         for (RoadArea roadArea:list){
@@ -48,7 +47,43 @@ public class DemoGenerator extends EntityGenerator{
             i++;
         }
 
+        //Office generator
+        List<Office> office = new ArrayList<>();
 
+        Office office1 = new Office(0, 40, 8, 6,1, map,Direction.SOUTH);
+        office.add(office1);
+
+        Office office2 = new Office(26, 24, 6, 6,1, map,Direction.EAST);
+        office.add(office2);
+
+        Office office3 = new Office(52, 28, 6, 8,1, map,Direction.NORTH);
+        office.add(office3);
+
+        Office office4 = new Office(52, 4, 4, 8,1, map,Direction.NORTH);
+        office.add(office4);
+
+
+        //Park generator
+        List<Park> park = new ArrayList<>();
+        Park park1 = new Park(40, 40, 8, 20, 1, map, Direction.SOUTH);
+        park.add(park1);
+
+        //School generator
+        List<School> school = new ArrayList<>();
+        School school1 = new School(4, 24, 12, 8, 1, map, Direction.EAST);
+        school.add(school1);
+
+        //Restaurant generator
+        List<Restaurant> restaurant = new ArrayList<>();
+        Restaurant restaurant1 = new Restaurant(26, 16, 4, 6, 1, map, Direction.EAST);
+        restaurant.add(restaurant1);
+
+        Restaurant restaurant2 = new Restaurant(40, 28, 6, 12, 1, map, Direction.SOUTH);
+        restaurant.add(restaurant2);
+
+        //Mall generator
+        List<Mall> mall = new ArrayList<>();
+        Mall mall1 = new Mall(40, 18, 10, 20,1, map,Direction.NORTH);
 
         return null;
     }
